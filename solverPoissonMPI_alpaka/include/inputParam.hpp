f9ad5708c75c112963c1aef23585aa5423d8cb6e
Luca <pennati@kth.se>
2024-12-17 13:04:52 +0100

    alpaka

--- solverPoissonMPI_alpaka/include/inputParam.hpp
+++ solverPoissonMPI_alpaka/include/inputParam.hpp
@@ -32,7 +32,8 @@ using T_Solver = BiCGstabAlpaka<DIM, T_data, tollMainSolver, iterMaxMainSolver, 
 constexpr std::array<int,3> npglobal={128,128,128};
 constexpr std::array<T_data,3> ds={0.1,0.1,0.1};
 constexpr std::array<T_data,3> origin={3,2.5,10};
-constexpr std::array<int,6> bcsType={0,1,1,0,1,0}; //0 Dirichlet, 1 Neumann, -1 No dim; order x-x+ y-y+ z-z+
+//constexpr std::array<int,6> bcsType={0,1,1,0,1,0}; //0 Dirichlet, 1 Neumann, -1 No dim; order x-x+ y-y+ z-z+
+constexpr std::array<int,6> bcsType={0,0,0,0,0,0}; //0 Dirichlet, 1 Neumann, -1 No dim; order x-x+ y-y+ z-z+
 constexpr std::array<T_data,6> bcsValue={0,0,0,0,0,0}; //0 Dirichlet, 1 Neumann, -1 No dim
 
 
